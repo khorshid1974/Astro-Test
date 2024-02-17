c058363b3fa1aef1f22509f9dad53f4186a6957b
khorshid1974 <khorshid.hamza@epu.edu.iq>
2024-02-17 18:58:30 +0300

    Astro test

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from 'astro:content';
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+const productCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+   blog: blogCollection, 
+   products: productCollection
+};
